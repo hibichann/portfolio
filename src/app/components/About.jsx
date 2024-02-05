f46f1fd6346f85e4e1ff3308dea7acac70ab7599
hibi <coder101011@outlook.com>
2024-02-05 22:23:25 +0800

    commit

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -8,17 +8,17 @@ const About = () => {
 					<Image
 						className='place-self-center'
 						src='/images/about.jpg'
-						width={300}
-						height={300}
+						width={ 300 }
+						height={ 300 }
 						alt='CSS'></Image>
 				</div>
 				<div>
-					<h2>About me</h2>
+					<h2 className="text-4xl font-bold mb-4">About me</h2>
 					<p>
-						I am I am I am I am I am I am I am I am I am I am I am I am I am I
-						am I am I am I am I am I am I am I am I am I am I am I am I am I am
-						I am I am I am I am I am I am I am I am I am I am I am I am I am I
-						am I am I am I am I am I am I am I am
+						全职前端开发者，致力于创造用户友好的UI界面，并提供丝滑的浏览体验。
+					</p>
+					<p>
+						使用Vue/React构建现代化的网站。
 					</p>
 				</div>
 			</div>
